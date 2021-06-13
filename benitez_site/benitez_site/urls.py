9e460da2161124190565d5b7d051ed1ab7a386fd
Eduardo Benitez <eduardombc96@gmail.com>
2021-06-13 09:54:14 -0500

    Added urls structure.

--- benitez_site/benitez_site/urls.py
+++ benitez_site/benitez_site/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include("blog.urls")) # http://localhost:8000/blog/
+    # path('', include(homepage.urls)),  # http://localhost:8000/
+    # path('league-highlights/', include("league-highlights.urls")), # http://localhost:8000/league-highlights/
+    # path('personal-goals/', include("personal-goals.urls")) # http://localhost:8000/personal-goals/
 ]
